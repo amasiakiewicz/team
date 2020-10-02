e9286e25475132d97e38ffb1db2f84fc84528af3
Arkadiusz Masiakiewicz <arkadiusz.masiakiewicz@avra.pl>
2020-10-02 17:15:53 +0200

    adding swagger doc

--- src/main/java/com/casinoroyale/team/TeamApplication.java
+++ src/main/java/com/casinoroyale/team/TeamApplication.java
@@ -14,6 +14,10 @@ import org.joda.money.Money;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class TeamApplication {
@@ -42,6 +46,15 @@ public class TeamApplication {
 		mapper.registerModule(javaTimeModule);
 
 		return mapper;
+	}
+
+	@Bean
+	Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
